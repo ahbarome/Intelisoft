a82de814ab56f0b12e31d7f7a1c7954aee8c09bf
ahbarome <ahbarome@gmail.com>
2013-05-13 15:36:33 -0500

    Se cargan las fuentes de la aplicacion para el control de acceso

---
+++ SOURCE CODE/BMS.APPLICATION/BMS.APPLICATION/ConnectivityApplication.cs
@@ -0,0 +1,275 @@
+﻿/**********************************************************
+ * Demo for Standalone SDK.Created by Darcy on Oct.15 2009*
+***********************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace RTEvents
+{
+    public partial class ConnectivityApplication : Form
+    {
+        BMS.APPLICATION.BUSINESSLOGIC.BiometricProcess business;        
+        public ConnectivityApplication()
+        {
+            InitializeComponent();
+            
+            business = new BMS.APPLICATION.BUSINESSLOGIC.BiometricProcess();
+
+            this.label4.Text = ConfigurationManager.AppSettings["ProcessType"].ToString();
+        }
+
+        bool canEntry = false;
+        //Create Standalone SDK class dynamicly.
+        public zkemkeeper.CZKEMClass axCZKEM1 = new zkemkeeper.CZKEMClass();
+
+
+        /******************************************************************************************************************************************
+        * 
+        * 
+        * 
+        * ****************************************************************************************************************************************/
+        #region Communication
+        private bool bIsConnected = false; //
+        private int iMachineNumber = 1;//
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (txtIP.Text.Trim() == "" || txtPort.Text.Trim() == "")
+            {
+                MessageBox.Show("IP and Port cannot be null", "Error");
+                return;
+            }
+            int idwErrorCode = 0;
+
+            Cursor = Cursors.WaitCursor;
+            if (btnConnect.Text == "DisConnect")
+            {
+                axCZKEM1.Disconnect();
+
+                this.axCZKEM1.OnFinger -= new zkemkeeper._IZKEMEvents_OnFingerEventHandler(axCZKEM1_OnFinger);
+                this.axCZKEM1.OnVerify -= new zkemkeeper._IZKEMEvents_OnVerifyEventHandler(axCZKEM1_OnVerify);
+                this.axCZKEM1.OnAttTransactionEx -= new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
+                this.axCZKEM1.OnFingerFeature -= new zkemkeeper._IZKEMEvents_OnFingerFeatureEventHandler(axCZKEM1_OnFingerFeature);
+                this.axCZKEM1.OnEnrollFingerEx -= new zkemkeeper._IZKEMEvents_OnEnrollFingerExEventHandler(axCZKEM1_OnEnrollFingerEx);
+                this.axCZKEM1.OnDeleteTemplate -= new zkemkeeper._IZKEMEvents_OnDeleteTemplateEventHandler(axCZKEM1_OnDeleteTemplate);
+                this.axCZKEM1.OnNewUser -= new zkemkeeper._IZKEMEvents_OnNewUserEventHandler(axCZKEM1_OnNewUser);
+                this.axCZKEM1.OnHIDNum -= new zkemkeeper._IZKEMEvents_OnHIDNumEventHandler(axCZKEM1_OnHIDNum);
+                this.axCZKEM1.OnAlarm -= new zkemkeeper._IZKEMEvents_OnAlarmEventHandler(axCZKEM1_OnAlarm);
+                this.axCZKEM1.OnDoor -= new zkemkeeper._IZKEMEvents_OnDoorEventHandler(axCZKEM1_OnDoor);
+                this.axCZKEM1.OnWriteCard -= new zkemkeeper._IZKEMEvents_OnWriteCardEventHandler(axCZKEM1_OnWriteCard);
+                this.axCZKEM1.OnEmptyCard -= new zkemkeeper._IZKEMEvents_OnEmptyCardEventHandler(axCZKEM1_OnEmptyCard);
+
+                bIsConnected = false;
+                btnConnect.Text = "Connect";
+                lblState.Text = "Current State:DisConnected";
+                Cursor = Cursors.Default;
+                return;
+            }
+
+            bIsConnected = axCZKEM1.Connect_Net(txtIP.Text, Convert.ToInt32(txtPort.Text));
+
+            if (bIsConnected == true)
+            {
+                btnConnect.Text = "DisConnect";
+                btnConnect.Refresh();
+                lblState.Text = "Current State:Connected";
+                iMachineNumber = 1;//In fact,when you are using the tcp/ip communication,this parameter will be ignored,that is any integer will all right.Here we use 1.
+                if (axCZKEM1.RegEvent(iMachineNumber, 65535))//Here you can register the realtime events that you want to be triggered(the parameters 65535 means registering all)
+                {
+                    this.axCZKEM1.OnFinger += new zkemkeeper._IZKEMEvents_OnFingerEventHandler(axCZKEM1_OnFinger);
+                    this.axCZKEM1.OnVerify += new zkemkeeper._IZKEMEvents_OnVerifyEventHandler(axCZKEM1_OnVerify);
+                    this.axCZKEM1.OnAttTransactionEx += new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
+                    this.axCZKEM1.OnFingerFeature += new zkemkeeper._IZKEMEvents_OnFingerFeatureEventHandler(axCZKEM1_OnFingerFeature);
+                    this.axCZKEM1.OnEnrollFingerEx += new zkemkeeper._IZKEMEvents_OnEnrollFingerExEventHandler(axCZKEM1_OnEnrollFingerEx);
+                    this.axCZKEM1.OnDeleteTemplate += new zkemkeeper._IZKEMEvents_OnDeleteTemplateEventHandler(axCZKEM1_OnDeleteTemplate);
+                    this.axCZKEM1.OnNewUser += new zkemkeeper._IZKEMEvents_OnNewUserEventHandler(axCZKEM1_OnNewUser);
+                    this.axCZKEM1.OnHIDNum += new zkemkeeper._IZKEMEvents_OnHIDNumEventHandler(axCZKEM1_OnHIDNum);
+                    this.axCZKEM1.OnAlarm += new zkemkeeper._IZKEMEvents_OnAlarmEventHandler(axCZKEM1_OnAlarm);
+                    this.axCZKEM1.OnDoor += new zkemkeeper._IZKEMEvents_OnDoorEventHandler(axCZKEM1_OnDoor);
+                    this.axCZKEM1.OnWriteCard += new zkemkeeper._IZKEMEvents_OnWriteCardEventHandler(axCZKEM1_OnWriteCard);
+                    this.axCZKEM1.OnEmptyCard += new zkemkeeper._IZKEMEvents_OnEmptyCardEventHandler(axCZKEM1_OnEmptyCard);
+                }
+            }
+            else
+            {
+                axCZKEM1.GetLastError(ref idwErrorCode);
+                MessageBox.Show("Unable to connect the device, ErrorCode=" + idwErrorCode.ToString(), "Error");
+            }
+            Cursor = Cursors.Default;
+        }
+
+        #endregion
+
+
+        #region RealTime Events
+
+        //When you place your finger on sensor of the device,this event will be triggered
+        private void axCZKEM1_OnFinger()
+        {
+            lbRTShow.Items.Add("BMS Connectivy Application OnFinger Has been Triggered");
+            
+            if (this.canEntry)
+            {
+                axCZKEM1.Disconnect();
+                this.canEntry = axCZKEM1.Connect_Net(txtIP.Text, Convert.ToInt32(txtPort.Text));
+            }
+
+        }
+
+        //After you have placed your finger on the sensor(or swipe your card to the device),this event will be triggered.
+        //If you passes the verification,the returned value userid will be the user enrollnumber,or else the value will be -1;
+        private void axCZKEM1_OnVerify(int iUserID)
+        {
+            lbRTShow.Items.Add("BMS Connectivy Application OnVerify Has been Triggered,Verifying...");
+            if (iUserID != -1)
+            {
+                lbRTShow.Items.Add("Verified OK,the UserID is " + iUserID.ToString());
+            }
+            else
+            {
+                lbRTShow.Items.Add("Verified Failed... ");
+            }
+        }
+
+        //If your fingerprint(or your card) passes the verification,this event will be triggered
+        private void axCZKEM1_OnAttTransactionEx(string sEnrollNumber, int iIsInValid, int iAttState, int iVerifyMethod, int iYear, int iMonth, int iDay, int iHour, int iMinute, int iSecond, int iWorkCode)
+        {
+
+            string message = string.Empty;
+
+            canEntry = business.CanEntryPerson(sEnrollNumber, out message);
+
+            lbRTShow.Items.Add("BMS Connectivy Application OnAttTrasactionEx Has been Triggered,Verified OK");
+            lbRTShow.Items.Add(string.Concat("...UserID:", sEnrollNumber));
+            lbRTShow.Items.Add(string.Concat("...isInvalid:", iIsInValid.ToString()));
+            lbRTShow.Items.Add(string.Concat("...attState:", iAttState.ToString()));
+            lbRTShow.Items.Add(string.Concat("...VerifyMethod:", iVerifyMethod.ToString()));
+            lbRTShow.Items.Add(string.Concat("...Workcode:", iWorkCode.ToString()));//the difference between the event OnAttTransaction and OnAttTransactionEx
+            lbRTShow.Items.Add(string.Concat("...Time:", iYear.ToString(), "-", iMonth.ToString(), "-", iDay.ToString(), " ", iHour.ToString(), ":", iMinute.ToString(), ":", iSecond.ToString()));
+            lbRTShow.Items.Add(string.Concat("...Can Entry:", canEntry));
+            lbRTShow.Items.Add(string.Concat("...Aditional Information:", message));
+
+            if (canEntry)
+            {
+                axCZKEM1.ACUnlock(int.Parse(ConfigurationManager.AppSettings["MachineNumber"].ToString()), int.Parse(ConfigurationManager.AppSettings["Delay"].ToString()));
+            }
+
+        }
+
+        //When you have enrolled your finger,this event will be triggered and return the quality of the fingerprint you have enrolled
+        private void axCZKEM1_OnFingerFeature(int iScore)
+        {
+            if (iScore < 0)
+            {
+                lbRTShow.Items.Add("The quality of your fingerprint is poor");
+            }
+            else
+            {
+                lbRTShow.Items.Add("BMS Connectivy Application OnFingerFeature Has been Triggered...Score:　" + iScore.ToString());
+            }
+        }
+
+        //When you are enrolling your finger,this event will be triggered.(The event can only be triggered by TFT screen devices)
+        private void axCZKEM1_OnEnrollFingerEx(string sEnrollNumber, int iFingerIndex, int iActionResult, int iTemplateLength)
+        {
+            if (iActionResult == 0)
+            {
+                lbRTShow.Items.Add("BMS Connectivy Application OnEnrollFigerEx Has been Triggered....");
+                lbRTShow.Items.Add(".....UserID: " + sEnrollNumber + " Index: " + iFingerIndex.ToString() + " tmpLen: " + iTemplateLength.ToString());
+            }
+            else
+            {
+                lbRTShow.Items.Add("BMS Connectivy Application OnEnrollFigerEx Has been Triggered Error,actionResult=" + iActionResult.ToString());
+            }
+        }
+
+        //When you have deleted one one fingerprint template,this event will be triggered.
+        private void axCZKEM1_OnDeleteTemplate(int iEnrollNumber, int iFingerIndex)
+        {
+            lbRTShow.Items.Add("BMS Connectivy Application OnDeleteTemplate Has been Triggered...");
+            lbRTShow.Items.Add("...UserID=" + iEnrollNumber.ToString() + " FingerIndex=" + iFingerIndex.ToString());
+        }
+
+        //When you have enrolled a new user,this event will be triggered.
+        private void axCZKEM1_OnNewUser(int iEnrollNumber)
+        {
+            lbRTShow.Items.Add("BMS Connectivy Application OnNewUser Has been Triggered...");
+            lbRTShow.Items.Add("...NewUserID=" + iEnrollNumber.ToString());
+        }
+
+        //When you swipe a card to the device, this event will be triggered to show you the card number.
+        private void axCZKEM1_OnHIDNum(int iCardNumber)
+        {
+            lbRTShow.Items.Add("BMS Connectivy Application OnHIDNum Has been Triggered...");
+            lbRTShow.Items.Add("...Cardnumber=" + iCardNumber.ToString());
+        }
+
+        //When the dismantling machine or duress alarm occurs, trigger this event.
+        private void axCZKEM1_OnAlarm(int iAlarmType, int iEnrollNumber, int iVerified)
+        {
+            lbRTShow.Items.Add("RTEvnet OnAlarm Has been Triggered...");
+            lbRTShow.Items.Add("...AlarmType=" + iAlarmType.ToString());
+            lbRTShow.Items.Add("...EnrollNumber=" + iEnrollNumber.ToString());
+            lbRTShow.Items.Add("...Verified=" + iVerified.ToString());
+        }
+
+        //Door sensor event
+        private void axCZKEM1_OnDoor(int iEventType)
+        {
+            lbRTShow.Items.Add("BMS Connectivy Application Ondoor Has been Triggered...");
+            lbRTShow.Items.Add("...EventType=" + iEventType.ToString());
+        }
+
+        //When you have emptyed the Mifare card,this event will be triggered.
+        private void axCZKEM1_OnEmptyCard(int iActionResult)
+        {
+            lbRTShow.Items.Add("BMS Connectivy Application OnEmptyCard Has been Triggered...");
+            if (iActionResult == 0)
+            {
+                lbRTShow.Items.Add("...Empty Mifare Card OK");
+            }
+            else
+            {
+                lbRTShow.Items.Add("...Empty Failed");
+            }
+        }
+
+        //When you have written into the Mifare card ,this event will be triggered.
+        private void axCZKEM1_OnWriteCard(int iEnrollNumber, int iActionResult, int iLength)
+        {
+            lbRTShow.Items.Add("BMS Connectivy Application OnWriteCard Has been Triggered...");
+            if (iActionResult == 0)
+            {
+                lbRTShow.Items.Add("...Write Mifare Card OK");
+                lbRTShow.Items.Add("...EnrollNumber=" + iEnrollNumber.ToString());
+                lbRTShow.Items.Add("...TmpLength=" + iLength.ToString());
+            }
+            else
+            {
+                lbRTShow.Items.Add("...Write Failed");
+            }
+        }
+
+        //After function GetRTLog() is called ,RealTime Events will be triggered. 
+        //When you are using these two functions, it will request data from the device forwardly.
+        private void rtTimer_Tick(object sender, EventArgs e)
+        {
+            if (axCZKEM1.ReadRTLog(iMachineNumber))
+            {
+                while (axCZKEM1.GetRTLog(iMachineNumber))
+                {
+                    ;
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
